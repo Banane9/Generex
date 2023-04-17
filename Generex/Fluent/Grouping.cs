088bb27a01458c9c9f1be3aa1f2cf75aae4bef02
Arne Kiesewetter <banane9@live.de>
2023-04-17 20:00:07 +0200

    Add documentation to the Fluent.Grouping interfaces

--- Generex/Fluent/Grouping.cs
+++ Generex/Fluent/Grouping.cs
@@ -4,52 +4,192 @@ using System.Text;
 
 namespace Generex.Fluent
 {
+    /// <summary>
+    /// The options for an atomic group, which is part of a list of alternatives.
+    /// </summary>
+    /// <typeparam name="T">The type of elements in the input sequence.</typeparam>
     public interface IAlternativeAtomicGroup<T>
     {
+        /// <summary>
+        /// Finish constructing a plain atomic group, which prevents backtracking into it.
+        /// </summary>
         IAlternativeAtom<T> Group { get; }
+
+        /// <summary>
+        /// Finish constructing a non-capturing atomic group, which excludes its contents from the final match.
+        /// </summary>
         IAlternativeAtom<T> NonCapturingGroup { get; }
 
+        /// <summary>
+        /// Construct a not yet explicitly named atomic capture group, creating a new <see cref="CaptureReference{T}"/>.
+        /// </summary>
+        /// <param name="captureReference">Outputs the capture group's reference.</param>
+        /// <returns>The not yet explicitly named atomic capture group.</returns>
         IAlternativeUnnamedCapturedAtom<T> CapturingGroup(out CaptureReference<T> captureReference);
+
+        /// <summary>
+        /// Construct an atomic capture group with an existing <see cref="CaptureReference{T}"/>.
+        /// </summary>
+        /// <param name="captureReference">The reference to use.</param>
+        /// <returns>The fully configured atomic capture group.</returns>
+        IAlternativeCapturedAtom<T> CapturingGroup(CaptureReference<T> captureReference);
     }
 
+    /// <summary>
+    /// The options for a group, which is part of a list of alternatives.
+    /// </summary>
+    /// <typeparam name="T">The type of elements in the input sequence.</typeparam>
     public interface IAlternativeGroup<T>
     {
+        /// <summary>
+        /// Construct an atomic group, which prevents backtracking into it.
+        /// </summary>
         IAlternativeAtomicGroup<T> Atomic { get; }
+
+        /// <summary>
+        /// Construct a non-capturing group, which excludes its contents from the final match.
+        /// </summary>
         IAlternativeAtom<T> NonCapturingGroup { get; }
 
+        /// <summary>
+        /// Construct a not yet explicitly named capture group, creating a new <see cref="CaptureReference{T}"/>.
+        /// </summary>
+        /// <param name="captureReference">Outputs the capture group's reference.</param>
+        /// <returns>The not yet explicitly named capture group.</returns>
         IAlternativeUnnamedCapturedAtom<T> CapturingGroup(out CaptureReference<T> captureReference);
+
+        /// <summary>
+        /// Construct an capture group with an existing <see cref="CaptureReference{T}"/>.
+        /// </summary>
+        /// <param name="captureReference">The reference to use.</param>
+        /// <returns>The fully configured capture group.</returns>
+
+        IAlternativeCapturedAtom<T> CapturingGroup(CaptureReference<T> captureReference);
     }
 
+    /// <summary>
+    /// The options for an atomic group,
+    /// which is not yet part of a sequence or list of alternatives.
+    /// </summary>
+    /// <typeparam name="T">The type of elements in the input sequence.</typeparam>
     public interface IAtomicGroup<T>
     {
+        /// <summary>
+        /// Finish constructing a plain atomic group, which prevents backtracking into it.
+        /// </summary>
         IAtom<T> Group { get; }
+
+        /// <summary>
+        /// Finish constructing a non-capturing atomic group, which excludes its contents from the final match.
+        /// </summary>
         IAtom<T> NonCapturingGroup { get; }
 
+        /// <summary>
+        /// Construct a not yet explicitly named atomic capture group, creating a new <see cref="CaptureReference{T}"/>.
+        /// </summary>
+        /// <param name="captureReference">Outputs the capture group's reference.</param>
+        /// <returns>The not yet explicitly named atomic capture group.</returns>
         IUnnamedCapturedAtom<T> CapturingGroup(out CaptureReference<T> captureReference);
+
+        /// <summary>
+        /// Construct an atomic capture group with an existing <see cref="CaptureReference{T}"/>.
+        /// </summary>
+        /// <param name="captureReference">The reference to use.</param>
+        /// <returns>The fully configured atomic capture group.</returns>
+        ICapturedAtom<T> CapturingGroup(CaptureReference<T> captureReference);
     }
 
+    /// <summary>
+    /// The options for a group, which is not yet part of a sequence or list of alternatives.
+    /// </summary>
+    /// <typeparam name="T">The type of elements in the input sequence.</typeparam>
     public interface IGroup<T>
     {
+        /// <summary>
+        /// Construct an atomic group, which prevents backtracking into it.
+        /// </summary>
         IAtomicGroup<T> Atomic { get; }
+
+        /// <summary>
+        /// Construct a non-capturing group, which excludes its contents from the final match.
+        /// </summary>
         IAtom<T> NonCapturingGroup { get; }
 
+        /// <summary>
+        /// Construct a not yet explicitly named capture group, creating a new <see cref="CaptureReference{T}"/>.
+        /// </summary>
+        /// <param name="captureReference">Outputs the capture group's reference.</param>
+        /// <returns>The not yet explicitly named capture group.</returns>
         IUnnamedCapturedAtom<T> CapturingGroup(out CaptureReference<T> captureReference);
+
+        /// <summary>
+        /// Construct an capture group with an existing <see cref="CaptureReference{T}"/>.
+        /// </summary>
+        /// <param name="captureReference">The reference to use.</param>
+        /// <returns>The fully configured capture group.</returns>
+        ICapturedAtom<T> CapturingGroup(CaptureReference<T> captureReference);
     }
 
+    /// <summary>
+    /// The options for an atomic group, which is part of a sequence.
+    /// </summary>
+    /// <typeparam name="T">The type of elements in the input sequence.</typeparam>
     public interface ISequenceAtomicGroup<T>
     {
+        /// <summary>
+        /// Finish constructing a plain atomic group, which prevents backtracking into it.
+        /// </summary>
         ISequenceAtom<T> Group { get; }
+
+        /// <summary>
+        /// Finish constructing a non-capturing atomic group, which excludes its contents from the final match.
+        /// </summary>
         ISequenceAtom<T> NonCapturingGroup { get; }
 
+        /// <summary>
+        /// Construct a not yet explicitly named atomic capture group, creating a new <see cref="CaptureReference{T}"/>.
+        /// </summary>
+        /// <param name="captureReference">Outputs the capture group's reference.</param>
+        /// <returns>The not yet explicitly named atomic capture group.</returns>
         ISequenceUnnamedCapturedAtom<T> CapturingGroup(out CaptureReference<T> captureReference);
+
+        /// <summary>
+        /// Construct an atomic capture group with an existing <see cref="CaptureReference{T}"/>.
+        /// </summary>
+        /// <param name="captureReference">The reference to use.</param>
+        /// <returns>The fully configured atomic capture group.</returns>
+        ISequenceCapturedAtom<T> CapturingGroup(CaptureReference<T> captureReference);
     }
 
+    /// <summary>
+    /// The options for a group, which is part of a sequence.
+    /// </summary>
+    /// <typeparam name="T">The type of elements in the input sequence.</typeparam>
     public interface ISequenceGroup<T>
     {
+        /// <summary>
+        /// Construct an atomic group, which prevents backtracking into it.
+        /// </summary>
         ISequenceAtomicGroup<T> Atomic { get; }
+
+        /// <summary>
+        /// Construct a non-capturing group, which excludes its contents from the final match.
+        /// </summary>
         ISequenceAtom<T> NonCapturingGroup { get; }
 
+        /// <summary>
+        /// Construct a not yet explicitly named capture group, creating a new <see cref="CaptureReference{T}"/>.
+        /// </summary>
+        /// <param name="captureReference">Outputs the capture group's reference.</param>
+        /// <returns>The not yet explicitly named capture group.</returns>
         ISequenceUnnamedCapturedAtom<T> CapturingGroup(out CaptureReference<T> captureReference);
+
+        /// <summary>
+        /// Construct an capture group with an existing <see cref="CaptureReference{T}"/>.
+        /// </summary>
+        /// <param name="captureReference">The reference to use.</param>
+        /// <returns>The fully configured capture group.</returns>
+        ISequenceCapturedAtom<T> CapturingGroup(CaptureReference<T> captureReference);
     }
 
     internal class Grouping<T> : Atom<T>, IParentAtom<T>, IAtomicGroup<T>, IGroup<T>, IUnnamedCapturedAtom<T>,
@@ -128,6 +268,24 @@ namespace Generex.Fluent
 
         ISequenceUnnamedCapturedAtom<T> ISequenceGroup<T>.CapturingGroup(out CaptureReference<T> captureReference)
             => (ISequenceUnnamedCapturedAtom<T>)CapturingGroup(out captureReference);
+
+        public ICapturedAtom<T> CapturingGroup(CaptureReference<T> captureReference)
+        {
+            this.captureReference = captureReference;
+            return this;
+        }
+
+        IAlternativeCapturedAtom<T> IAlternativeAtomicGroup<T>.CapturingGroup(CaptureReference<T> captureReference)
+            => (IAlternativeCapturedAtom<T>)CapturingGroup(captureReference);
+
+        IAlternativeCapturedAtom<T> IAlternativeGroup<T>.CapturingGroup(CaptureReference<T> captureReference)
+            => (IAlternativeCapturedAtom<T>)CapturingGroup(captureReference);
+
+        ISequenceCapturedAtom<T> ISequenceAtomicGroup<T>.CapturingGroup(CaptureReference<T> captureReference)
+            => (ISequenceCapturedAtom<T>)CapturingGroup(captureReference);
+
+        ISequenceCapturedAtom<T> ISequenceGroup<T>.CapturingGroup(CaptureReference<T> captureReference)
+            => (ISequenceCapturedAtom<T>)CapturingGroup(captureReference);
 
         public IRepeatStart<T> WrapInGreedyRepeat(IFinishableAtom<T> child)
         {
