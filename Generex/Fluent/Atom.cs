e950842d218f1b9d18a80fae21bd5f30d02cce47
Arne Kiesewetter <banane9@live.de>
2023-04-17 19:59:34 +0200

    Add documentation to the Fluent.Atom interfaces

--- Generex/Fluent/Atom.cs
+++ Generex/Fluent/Atom.cs
@@ -4,35 +4,108 @@ using System.Text;
 
 namespace Generex.Fluent
 {
+    /// <summary>
+    /// The options for a general atom,
+    /// which is not yet part of a sequence or list of alternatives.
+    /// </summary>
+    /// <inheritdoc/>
     public interface IAtom<T> : IFinishableAtom<T>
     {
+        /// <summary>
+        /// Start a list of alternatives with this atom and add another to it.
+        /// </summary>
         IAlternativeNext<T> Alternatively { get; }
+
+        /// <summary>
+        /// Construct a group wrapper for the current atom.
+        /// </summary>
         IGroup<T> As { get; }
+
+        /// <summary>
+        /// Start a sequence with this atom and add the next one to it.
+        /// </summary>
         ISequenceNext<T> FollowedBy { get; }
+
+        /// <summary>
+        /// Construct a greedy quantifier for the current atom.
+        /// </summary>
         IRepeatStart<T> GreedilyRepeat { get; }
+
+        /// <summary>
+        /// Construct a lazy quantifier for the current atom.
+        /// </summary>
         IRepeatStart<T> LazilyRepeat { get; }
     }
 
+    /// <summary>
+    /// The options for a named capture group,
+    /// which is not yet part of a sequence or list of alternatives.
+    /// </summary>
+    /// <inheritdoc/>
     public interface ICapturedAtom<T> : IFinishableAtom<T>
     {
+        /// <summary>
+        /// Start a list of alternatives with this atom and add another to it.
+        /// </summary>
         IAlternativeNext<T> Alternatively { get; }
+
+        /// <summary>
+        /// Start a sequence with this atom and add the next one to it.
+        /// </summary>
         ISequenceNext<T> FollowedBy { get; }
     }
 
+    /// <summary>
+    /// The options for a finishable fluent construction.
+    /// </summary>
+    /// <remarks>
+    /// This is a state that the fluent construction can be finished in.
+    /// </remarks>
+    /// <typeparam name="T">The type of elements in the input sequence.</typeparam>
     public interface IFinishableAtom<T>
     {
+        /// <summary>
+        /// Generates a <see cref="Generex{T}"/> pattern representing the current fluent construction.
+        /// </summary>
+        /// <returns>The pattern representing the fluent construction.</returns>
         Generex<T> Finish();
     }
 
+    /// <summary>
+    /// The options for a quantified atom,
+    /// which is not yet part of a sequence or list of alternatives.
+    /// </summary>
+    /// <inheritdoc/>
     public interface IRepeatedAtom<T> : IFinishableAtom<T>
     {
+        /// <summary>
+        /// Start a list of alternatives with this atom and add another to it.
+        /// </summary>
         IAlternativeNext<T> Alternatively { get; }
+
+        /// <summary>
+        /// Construct a group wrapper for the current atom.
+        /// </summary>
         IGroup<T> As { get; }
+
+        /// <summary>
+        /// Start a sequence with this atom and add the next one to it.
+        /// </summary>
         ISequenceNext<T> FollowedBy { get; }
     }
 
+    /// <summary>
+    /// The options for a not yet explicitly named capture group,
+    /// which is not yet part of a sequence or list of alternatives.
+    /// </summary>
+    /// <inheritdoc/>
     public interface IUnnamedCapturedAtom<T> : ICapturedAtom<T>
     {
+        /// <summary>
+        /// Adds a name to the capture group.
+        /// </summary>
+        /// <param name="name">The name to associate with the group.</param>
+        /// <returns>The named capture group.</returns>
         ICapturedAtom<T> Called(string name);
     }
 
